05aeb13450493568540e3744b49bd1980db1cdd1
rudwnok123 <rudwnok123@naver.com>
2018-12-07 17:28:52 +0900

    package update?

--- public/qna.js
+++ public/qna.js
@@ -1,9 +1,9 @@
 function $(selector) {
-    return document.querySelector(selector);
+  return document.querySelector(selector);
 }
 
-function appendAnswer({content, writer, date, answerId}) {
-    const commentHTML = `
+function appendAnswer({ content, writer, date, answerId }) {
+  const commentHTML = `
     <li class="answer" data-id=${answerId}>
         <div class="answer-content"> ${content} </div>
         <div class="answer-metainfo">
@@ -15,14 +15,54 @@ function appendAnswer({content, writer, date, answerId}) {
         </div>
     </li> `
 
-    return commentHTML;
+  return commentHTML;
 }
 
 
 function initEvents() {
-    //이벤트등록
+  //이벤트등록
 }
 
+const id = {
+  user: 'crong'
+}
+
+function login(id) {
+  fetch('/api/login', {
+    method: 'POST',
+    // headers: JSON.stringify(id)
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'appliction/json'
+      // user : "crong"
+    },
+    body: JSON.stringify({
+      id
+    })
+  })
+    .then((response) => {
+      return response
+    })
+    // .then(function (data) {
+    //   console.log('cc', data);
+    // })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  console.log('login');
+}
+
+function logout() { }
+
 document.addEventListener("DOMContentLoaded", () => {
-    initEvents();
+  initEvents();
+})
+
+
+
+$('.login-btn').addEventListener('click', () => {
+  login(id);
 })
